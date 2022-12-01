375dc2d79d6e2952c29adada1f5fa70f85111a8a
Tejas Mane <tejasmane485@gmail.com>
2022-12-01 16:56:20 +0530

    helper fixed

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,15 +1,14 @@
-package helpers
+package helper
 
-import (
+import(
 	"errors"
-
 	"github.com/gin-gonic/gin"
 )
 
-func CheckUserType(c *gin.Context, role string) (err error) {
+
+func CheckUserType(c *gin.Context, role string) (err error){
 	userType := c.GetString("user_type")
 	err = nil
-
 	if userType != role {
 		err = errors.New("Unauthorized to access this resource")
 		return err
@@ -17,16 +16,15 @@ func CheckUserType(c *gin.Context, role string) (err error) {
 	return err
 }
 
-func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error){
 	userType := c.GetString("user_type")
 	uid := c.GetString("uid")
-	err = nil
+	err= nil
 
 	if userType == "USER" && uid != userId {
 		err = errors.New("Unauthorized to access this resource")
 		return err
 	}
-
 	err = CheckUserType(c, userType)
 	return err
 }
