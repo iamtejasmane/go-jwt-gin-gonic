91206a4489318864650608500d43758d7b1750f7
Tejas Mane <tejasmane485@gmail.com>
2022-12-01 16:52:34 +0530

    env var added

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	routes "github.com/iamtejasmane/go-jwt-gin-gonic/routes"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		port = "8080"
+		port = "8000"
 	}
+
 	router := gin.New()
 	router.Use(gin.Logger())
 
